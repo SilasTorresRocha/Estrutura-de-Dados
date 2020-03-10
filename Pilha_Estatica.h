43a08c0e3a6468c7e184db326a2d08f40c993227
Silas Torres Rocha <43487074+SilasTorresRocha@users.noreply.github.com>
2020-03-10 15:07:02 -0300

    Create Pilha_Estatica.h

---
+++ Pilha_Estatica.h
@@ -0,0 +1,65 @@
+//
+// Created by Silas Torres Rocha on 10/03/2020.
+//
+
+#ifndef UNTITLED_PILHA_ESTATICA_H
+#define UNTITLED_PILHA_ESTATICA_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#define MAXTAM 100
+
+typedef struct {
+    int chave;
+} Objeto;
+
+typedef struct {
+    Objeto array[MAXTAM];
+    int topo;
+} pilhaEstatica;
+
+
+void iniciaPilha(pilhaEstatica *pilha){
+    pilha->topo=0;
+    return;
+}
+bool estaVazia(pilhaEstatica *pilha){
+    return(pilha->topo==0);
+}
+bool estaCheia(pilhaEstatica *pilha){
+    return(pilha->topo==0);
+}
+void empilha(Objeto item, pilhaEstatica *pilha){
+    if(pilha->topo==(MAXTAM-1)){
+        printf("Pilha cheia\n");
+        return;
+    }else{
+      pilha->array[pilha->topo].chave=item.chave;
+      pilha->topo++;
+        return;
+    }
+}
+void desempilha(pilhaEstatica *pilha){
+    pilha->topo--;
+    printf("Removido: %i\n",pilha->array[pilha->topo].chave);
+    return;
+}
+int tamanhoPilha(pilhaEstatica *pilha){
+    return (pilha->topo);
+}
+Objeto topo(pilhaEstatica *pilha){
+    return (pilha->array[pilha->topo]);
+}
+void imprimePilha(pilhaEstatica *pilha){
+    printf("{ ");
+    for (int i = 0; i < pilha->topo; ++i) {
+        printf("%i ",pilha->array[i].chave);
+    }
+    printf("}\n");
+    printf("O tamanho da pilha e de %i\nUso:%.1f%%\n",tamanhoPilha(pilha), ((float)pilha->topo/100.0)*100);
+    return;
+}
+
+#endif
+
